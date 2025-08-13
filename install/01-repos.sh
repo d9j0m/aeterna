c48901b34c2ae3010b774baf928549c3bb208c20
d9j0m <david.j.mcbrayer@gmail.com>
2025-08-13 15:26:19 -0400

    Switched to dnf makecache.

--- install/01-repos.sh
+++ install/01-repos.sh
@@ -5,11 +5,11 @@ echo -e "\nConfiguring Fedora Repos..."
 if ! dnf repolist | grep -q "rpmfusion-free"; then
   echo "Enabling RPM Fusion Free repository..."
   sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
-  sudo dnf groupupdate core
+  sudo dnf makecache
 fi
 
 if ! dnf repolist | grep -q "rpmfusion-nonfree"; then
   echo "Enabling RPM Fusion Nonfree repository..."
   sudo dnf install -y https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-  sudo dnf groupupdate core
+  sudo dnf makecache
 fi
