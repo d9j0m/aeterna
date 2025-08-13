a0c96665d6b637c6315bbefae266460201d94836
d9j0m <david.j.mcbrayer@gmail.com>
2025-08-13 17:24:50 -0400

    Added 02-dotfiles.sh

---
+++ install/02-dotfiles.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+echo -e "\n--Copying .bashrc to ~/.bashrc
+
+cp -R ~/.local/share/aeterna/default/bashrc ~/.bashrc
+
+echo -e "\n--Copying dotfiles to ~/.config/"
+
+cp -R ~/.local/share/aeterna/config/* ~/.config/
