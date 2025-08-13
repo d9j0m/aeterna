cf0095b3ebf7614a13e9c8821acd885035224615
d9j0m <david.j.mcbrayer@gmail.com>
2025-08-13 15:13:53 -0400

    Added RPMfusion Free Repo.

--- install/01-repos.sh
+++ install/01-repos.sh
@@ -2,4 +2,7 @@
 
 echo -e "\nConfiguring Fedora Repos..."
 
-
+if ! dnf repolist | grep -q "rpmfusion-free"; then
+  echo "Enabling RPM Fusion Free repository..."
+  sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+fi
